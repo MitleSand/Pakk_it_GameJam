900a87c7c66862cad8d9cd3f54639e37447f040f
MitleSand <132821586+MitleSand@users.noreply.github.com>
2025-01-06 15:50:39 +0100

    Packboyscript
    not done

---
+++ Assets/Asgeir/A_Scripts/PackBoyScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PackBoyScript : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public bool revealedPackboy = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!revealedPackboy)
+            {
+                animator.SetBool("PackboyReveal", true);
+            }
+
+            if (revealedPackboy)
+            {
+                animator.SetBool("PackboyReveal", false);
+            }
+        }
+
+
+    }
+}
