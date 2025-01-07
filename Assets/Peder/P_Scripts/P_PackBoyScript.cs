b045bc4c6240d039b83ad35e7794aba62329523a
Lilo <peder0623@outlook.com>
2025-01-07 11:43:29 +0100

    new packboy

---
+++ Assets/Peder/P_Scripts/P_PackBoyScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class P_PackBoyScript : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public bool revealedPackboy = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!revealedPackboy)
+            {
+                animator.SetBool("packboyReveal", true);
+                Debug.Log("Reaveal Packboy");
+                
+            }
+
+            if (revealedPackboy)
+            {
+                animator.SetBool("packboyReveal", false);
+                Debug.Log("Unreveal Packboy");
+            }
+        }
+
+
+    }
+
+    private IEnumerator PackBoyDelay()
+    {
+        yield return new WaitForSeconds(2);
+    }
+}
