76c34c5419f5c310c4bcf5dd3295d66949983022
Lilo <peder0623@outlook.com>
2025-01-09 13:23:20 +0100

    Created testscene 2

--- Assets/Peder/P_Scripts/P_PackBoyScript.cs
+++ Assets/Peder/P_Scripts/P_PackBoyScript.cs
@@ -5,41 +5,45 @@ public class P_PackBoyScript : MonoBehaviour
 {
 
     public Animator animator;
-
     public bool revealedPackboy = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PackBoyAnimation();
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    private void PackBoyAnimation()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
         {
             if (!revealedPackboy)
             {
                 animator.SetBool("packboyReveal", true);
+                revealedPackboy = true;
                 Debug.Log("Reaveal Packboy");
+                PackBoyDelay();
                 
             }
-
-            if (revealedPackboy)
+            else
             {
                 animator.SetBool("packboyReveal", false);
+                revealedPackboy = false;
                 Debug.Log("Unreveal Packboy");
+                PackBoyDelay();
             }
         }
-
-
     }
+        
 
     private IEnumerator PackBoyDelay()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
     }
 }
