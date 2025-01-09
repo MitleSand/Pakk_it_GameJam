1cc6d9eb1e3870e3bde5402a0b1e5f39af1adeb5
MitleSand <132821586+MitleSand@users.noreply.github.com>
2025-01-09 20:35:04 +0100

    timerscript

--- Assets/Asgeir/A_Scripts/TimerController.cs
+++ Assets/Asgeir/A_Scripts/TimerController.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TimerController : MonoBehaviour
 {
 
 
     [SerializeField] private float timeCounter;
+    [SerializeField] private float countdownTimer = 120f;
     [SerializeField] private int minutes;
     [SerializeField] private int seconds;
+    [SerializeField] private bool isCountdown;
     [SerializeField] private TextMeshProUGUI timerText;
 
 
@@ -20,8 +23,40 @@ public class TimerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        timeCounter += Time.time;
-        minutes = Mathf.FloorToInt(timeCounter / 60f);
-        seconds = Mathf.FloorToInt(timeCounter - minutes * 60);
+        if (isCountdown && countdownTimer > 0)
+        {
+        countdownTimer -= Time.deltaTime;
+        minutes = Mathf.FloorToInt(countdownTimer / 60f);
+        seconds = Mathf.FloorToInt(countdownTimer - minutes * 60);
+        
+
+        } else if (!isCountdown)
+        {
+            timeCounter += Time.deltaTime;
+            minutes = Mathf.FloorToInt(timeCounter / 60f);
+            seconds = Mathf.FloorToInt(timeCounter - minutes * 60);
+            
+        }
+        
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+
+        if (countdownTimer <= 0)
+        {
+            ReloadCurrentScene();
+        }
+
+    }
+
+    // Method to reload the current scene
+    public void ReloadCurrentScene()
+    {
+        // Get the active scene
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // Reload the active scene by its name
+        SceneManager.LoadScene(currentScene.name);
+
+        Debug.Log("Scene reloaded: " + currentScene.name);
     }
 }
